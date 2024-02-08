db32e7344309dcaac87ff09038b2bf57d1794c0c
Tomisin Jenrola <tomisinjenrola@gmail.com>
2024-02-08 00:24:46 -0500

    implemented model inference method

--- oracle/src/main.rs
+++ oracle/src/main.rs
@@ -1,116 +1,142 @@
+use actix_cors::Cors;
+use actix_web::http;
+use jsonrpc_v2;
+use jsonrpc_v2::{Data, Params, Server};
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
+use std::error::Error;
+use std::process::{Command, Output};
 
-// // derived from https://github.com/paritytech/jsonrpsee/blob/master/examples/examples/cors_server.rs
-
-// //! This example adds upstream CORS layers to the RPC service,
-// //! with access control allowing requests from all hosts.
-
-// use hyper::Method;
-// use jsonrpsee::server::{RpcModule, Server};
-// use std::net::SocketAddr;
-// use tower_http::cors::{Any, CorsLayer};
-
-// #[tokio::main]
-// async fn main() -> anyhow::Result<()> {
-// 	tracing_subscriber::FmtSubscriber::builder()
-// 		.with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-// 		.try_init()
-// 		.expect("setting default subscriber failed");
-
-// 	// Start up a JSON-RPC server that allows cross origin requests.
-// 	let server_addr = run_server().await?;
-
-// 	// Print instructions for testing CORS from a browser.
-// 	println!("Run the following snippet in the developer console in any Website.");
-// 	println!(
-// 		r#"
-//         fetch("http://{}", {{
-//             method: 'POST',
-//             mode: 'cors',
-//             headers: {{ 'Content-Type': 'application/json' }},
-//             body: JSON.stringify({{
-//                 jsonrpc: '2.0',
-//                 method: 'say_hello',
-//                 id: 1
-//             }})
-//         }}).then(res => {{
-//             console.log("Response:", res);
-//             return res.text()
-//         }}).then(body => {{
-//             console.log("Response Body:", body)
-//         }});
-//     "#,
-// 		server_addr
-// 	);
-
-// 	futures::future::pending().await
-// }
-
-// async fn run_server() -> anyhow::Result<SocketAddr> {
-// 	// Add a CORS middleware for handling HTTP requests.
-// 	// This middleware does affect the response, including appropriate
-// 	// headers to satisfy CORS. Because any origins are allowed, the
-// 	// "Access-Control-Allow-Origin: *" header is appended to the response.
-// 	let cors = CorsLayer::new()
-// 		// Allow `POST` when accessing the resource
-// 		.allow_methods([Method::POST])
-// 		// Allow requests from any origin
-// 		.allow_origin(Any)
-// 		.allow_headers([hyper::header::CONTENT_TYPE]);
-// 	let middleware = tower::ServiceBuilder::new().layer(cors);
-
-// 	// The RPC exposes the access control for filtering and the middleware for
-// 	// modifying requests / responses. These features are independent of one another
-// 	// and can also be used separately.
-// 	// In this example, we use both features.
-// 	let server = Server::builder().set_http_middleware(middleware).build("127.0.0.1:0".parse::<SocketAddr>()?).await?;
-
-// 	let mut module = RpcModule::new(());
-// 	module.register_method("say_hello", |_, _| {
-// 		println!("say_hello method called!");
-// 		"Hello there!!"
-// 	})?;
-
-// 	let addr = server.local_addr()?;
-// 	let handle = server.start(module);
-
-// 	// In this example we don't care about doing shutdown so let's it run forever.
-// 	// You may use the `ServerHandle` to shut it down or manage it yourself.
-// 	tokio::spawn(handle.stopped());
-
-// 	Ok(addr)
-// }
-
-use jsonrpc_v2::{Data, Error, Params, Server};
-
-#[derive(serde::Deserialize)]
-struct TwoNums {
-    a: usize,
-    b: usize,
+#[derive(Serialize, Deserialize)]
+struct Value {
+    inner: String,
 }
 
-async fn add(Params(params): Params<TwoNums>) -> Result<usize, Error> {
-    Ok(params.a + params.b)
+#[derive(Serialize, Deserialize)]
+struct SingleForeignCallParam {
+    single: Value, // Rust convention is to use snake_case for field names
 }
 
-async fn sub(Params(params): Params<(usize, usize)>) -> Result<usize, Error> {
-    Ok(params.0 - params.1)
+#[derive(Serialize, Deserialize)]
+struct ArrayForeignCallParam {
+    #[serde(rename(serialize = "Array"))]
+    array: Vec<Value>,
 }
 
-async fn message(data: Data<String>) -> Result<String, Error> {
-    Ok(String::from(&*data))
+#[derive(Serialize, Deserialize)]
+enum ForeignCallParam {
+    Single(SingleForeignCallParam),
+    Array(ArrayForeignCallParam),
+}
+
+#[derive(Serialize, Deserialize)]
+struct ForeignCallResult {
+    values: Vec<ForeignCallParam>,
+}
+
+#[derive(Debug, Deserialize)]
+struct InferenceResponse {
+    report: bool,
+    result: f64,
+}
+
+async fn inference(
+    Params(params): Params<serde_json::Value>,
+) -> Result<ForeignCallResult, jsonrpc_v2::Error> {
+    println!("{params}");
+    
+    match serde_json::from_value::<SingleForeignCallParam>(params.clone()) {
+        Err(e) => {
+            eprintln!("Failed to parse inference params: {}", e);
+            return Err(jsonrpc_v2::Error::INVALID_PARAMS);
+        }
+        Ok(p) => {
+            let bytecode = p.single.inner;
+            match exec_python_model(bytecode) {
+                Ok(output) => {
+                    println!("Python Output: {:?}", output);
+
+                    let report_value = Value {
+                        inner: output.report.to_string(),
+                    };
+                    let result_value = Value {
+                        inner: output.result.to_string(),
+                    };
+                    let array_resp = ArrayForeignCallParam {
+                        array: vec![report_value, result_value],
+                    };
+
+                    Ok(ForeignCallResult {
+                        values: vec![ForeignCallParam::Array(array_resp)],
+                    })
+                }
+                Err(e) => {
+                    eprintln!("Error calling Python code: {}", e.to_string());
+                    Err(jsonrpc_v2::Error::INTERNAL_ERROR)
+                }
+            }
+        }
+    }
+}
+
+fn exec_python_model(bytecode: String) -> Result<InferenceResponse, Box<dyn Error>> {
+    // println!("bytecode:\n{bytecode}");
+
+    // Ensure the Python environment is set up
+    install_deps()?;
+
+    let output: Output = Command::new("python")
+        .arg("../ml/main.py")
+        .arg(bytecode)
+        .output()?;
+
+    if !output.status.success() {
+        let error_message = String::from_utf8(output.stderr)?;
+        eprintln!("Command failed with error: {}", error_message);
+        return Err(error_message.into());
+    }
+
+    let output_str = String::from_utf8(output.stdout)?;
+    println!("Raw Python Output: {}", output_str);
+
+    let parsed: InferenceResponse = from_str(&output_str)?;
+    Ok(parsed)
+}
+
+fn install_deps() -> Result<(), Box<dyn Error>> {
+    let pip_install = Command::new("sh")
+        .arg("-c")
+        .arg("pip install -r ../ml/requirements.txt")
+        .output()?;
+
+    if !pip_install.status.success() {
+        let error_message = String::from_utf8_lossy(&pip_install.stderr);
+        eprintln!("Failed to install Python dependencies: {}", error_message);
+        return Err("Failed to install Python dependencies".into());
+    }
+
+    Ok(())
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let rpc = Server::new()
         .with_data(Data::new(String::from("Hello!")))
-        .with_method("sub", sub)
-        .with_method("message", message)
+        .with_method("inference", inference)
         .finish();
 
     actix_web::HttpServer::new(move || {
         let rpc = rpc.clone();
-        actix_web::App::new().service(
+
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+
+        // let cors = Cors::permissive();
+
+        actix_web::App::new().wrap(cors).service(
             actix_web::web::service("/api")
                 .guard(actix_web::guard::Post())
                 .finish(rpc.into_web_service()),
