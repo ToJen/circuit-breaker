71fe258aa6e2fae25fffccf6b540d1a305662788
William Wang <bwilliamwang@gmail.com>
2024-02-09 00:10:04 -0500

    Working function calling MVP

--- frontend/packages/nextjs/app/chat.js
+++ frontend/packages/nextjs/app/chat.js
@@ -10,15 +10,31 @@ import {
   TypingIndicator,
 } from "@chatscope/chat-ui-kit-react";
 
+import OpenAI from "openai";
 import { useState } from "react";
 
 const API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
+const openai = new OpenAI({ apiKey: API_KEY, dangerouslyAllowBrowser: true });
+
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = {
   //  Explain things like you're talking to a software professional with 5 years of experience.
   role: "system",
   content: "Explain things like you're talking to a software professional with 2 years of experience.",
 };
+
+function getCurrentWeather(location, unit = "fahrenheit") {
+  alert(`searching for weather in ${location}, unit ${unit}`);
+  if (location.toLowerCase().includes("tokyo")) {
+    return JSON.stringify({ location: "Tokyo", temperature: "10", unit: "celsius" });
+  } else if (location.toLowerCase().includes("san francisco")) {
+    return JSON.stringify({ location: "San Francisco", temperature: "72", unit: "fahrenheit" });
+  } else if (location.toLowerCase().includes("paris")) {
+    return JSON.stringify({ location: "Paris", temperature: "22", unit: "fahrenheit" });
+  } else {
+    return JSON.stringify({ location, temperature: "unknown" });
+  }
+}
 
 function App() {
   const [messages, setMessages] = useState([
@@ -66,12 +82,34 @@ function App() {
     // Get the request body set up with the model we plan to use
     // and the messages which we formatted above. We add a system message in the front to'
     // determine how we want chatGPT to act.
+    const tools = [
+      {
+        type: "function",
+        function: {
+          name: "get_current_weather",
+          description: "Get the current weather in a given location",
+          parameters: {
+            type: "object",
+            properties: {
+              location: {
+                type: "string",
+                description: "The city and state, e.g. San Francisco, CA",
+              },
+              unit: { type: "string", enum: ["celsius", "fahrenheit"] },
+            },
+            required: ["location"],
+          },
+        },
+      },
+    ];
     const apiRequestBody = {
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-0125",
       messages: [
         systemMessage, // The system message DEFINES the logic of our chatGPT
         ...apiMessages, // The messages from our chat with ChatGPT
       ],
+      tools: tools,
+      tool_choice: "auto", // auto is default, but we'll be explicit
     };
 
     await fetch("https://api.openai.com/v1/chat/completions", {
@@ -85,16 +123,69 @@ function App() {
       .then(data => {
         return data.json();
       })
-      .then(data => {
-        console.log(data);
-        setMessages([
-          ...chatMessages,
-          {
-            message: data.choices[0].message.content,
-            sender: "ChatGPT",
-          },
-        ]);
+      .then(async data => {
+        const response = data;
+        console.log("messages: ", messages);
+        console.log("response: ", response);
+
+        const responseMessage = response.choices[0].message;
+
+        // Step 2: check if the model wanted to call a function
+        const toolCalls = responseMessage.tool_calls;
+        if (responseMessage.tool_calls) {
+          // Step 3: call the function
+          // Note: the JSON response may not always be valid; be sure to handle errors
+          const availableFunctions = {
+            get_current_weather: getCurrentWeather,
+          }; // only one function in this example, but you can have multiple
+          messages.push(responseMessage); // extend conversation with assistant's reply
+          for (const toolCall of toolCalls) {
+            const functionName = toolCall.function.name;
+            const functionToCall = availableFunctions[functionName];
+            const functionArgs = JSON.parse(toolCall.function.arguments);
+            const functionResponse = await functionToCall(functionArgs.location, functionArgs.unit);
+            console.log("functionResponse: ", functionResponse);
+            setMessages([
+              ...chatMessages,
+              {
+                tool_call_id: toolCall.id,
+                role: "tool",
+                name: functionName,
+                content: functionResponse,
+              },
+            ]);
+            const secondResponse = await openai.chat.completions.create({
+              model: "gpt-3.5-turbo-0125",
+              messages: [
+                systemMessage, // The system message DEFINES the logic of our chatGPT
+                ...apiMessages, // The messages from our chat with ChatGPT
+                {
+                  role: "assistant",
+                  content: functionResponse,
+                },
+              ],
+            }); // get a new response from the model where it can see the function response
+            console.log(messages);
+            console.log("secondResponse: ", secondResponse);
+            setMessages([
+              ...chatMessages,
+              {
+                message: secondResponse.choices[0].message.content,
+                sender: "ChatGPT",
+              },
+            ]);
+          }
+        } else {
+          setMessages([
+            ...chatMessages,
+            {
+              message: data.choices[0].message.content,
+              sender: "ChatGPT",
+            },
+          ]);
+        }
         setIsTyping(false);
+        console.log(messages);
       });
   }
 
@@ -108,7 +199,6 @@ function App() {
               typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
             >
               {messages.map((message, i) => {
-                console.log(message);
                 return <Message key={i} model={message} />;
               })}
             </MessageList>
