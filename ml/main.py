c476ab024557b450a46806cabc4ab1f09765c3e3
mrhouzlane <rhouzm@gmail.com>
2024-02-13 06:27:40 +0100

    fix error with JSON RPC

--- ml/main.py
+++ ml/main.py
@@ -1,7 +1,6 @@
 import json
-import typer
+import argparse
 from soli_check import check
-
 
 def main(bytecode: str):
     (report, result) = check(bytecode)
@@ -10,9 +9,12 @@ def main(bytecode: str):
         "result": result
     }
     out = json.dumps(response, indent=4)
-
     print(out)
 
-
 if __name__ == "__main__":
-    typer.run(main)
+    parser = argparse.ArgumentParser(description="Process some bytecode.")
+    parser.add_argument('bytecode', type=str, help='Bytecode string to be processed')
+    
+    args = parser.parse_args()
+    
+    main(args.bytecode)
