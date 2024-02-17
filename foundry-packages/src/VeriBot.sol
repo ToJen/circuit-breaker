8a02cb3dcfe57a6fc693fb20ef8829b0378ba857
mrhouzlane <rhouzm@gmail.com>
2024-02-17 16:50:36 +0100

    fix VeriBot contract

--- foundry-packages/src/VeriBot.sol
+++ foundry-packages/src/VeriBot.sol
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {BaseUltraVerifier} from "../circuits/contracts/circuits/plonk_vk.sol";
+import {BaseUltraVerifier} from "./plonk_vk.sol";
 
 contract VeriBot {
     BaseUltraVerifier public plonkVerifier;
-    mapping(uint256 => bool) public proofs; // map a proof id to a boolean
 
-    function verifyProof(bytes calldata _proof, bytes32[] memory _publicInputs) public view returns (bool) {
-        uint256 counter = 0;
-        proofs[counter]++;
-        return plonkVerifier.verifyProof(_proof, _inputs);
-    }
+    mapping(bytes32 => bool) public proofs;
 
-    function getProof(uint256 _id) public view returns (bool) {
-        return proofs[_id];
+    function verifyProof(bytes calldata _proof, bytes32[] memory _publicInputs) public returns (bool) {
+        bytes32 proofId = keccak256(abi.encodePacked(_proof, _publicInputs));
+        // require(!proofs[proofId], "Proof has already been verified.");
+
+        // Verify the proof with the plonkVerifier
+        bool verified = plonkVerifier.verify(_proof, _publicInputs);
+
+        // If verified, mark this proof as verified to prevent future submissions
+        if (verified) {
+            proofs[proofId] = true;
+        }
+
+        return verified;
     }
 }
