4f5d161a0f17fdf671cd9d297bf1869f0187920d
William Wang <bn.william.w@gmail.com>
2024-02-17 14:03:25 -0500

    ether js test

--- frontend/packages/nextjs/contracts/externalContracts.ts
+++ frontend/packages/nextjs/contracts/externalContracts.ts
@@ -3,7 +3,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 /**
  * @example
  * const externalContracts = {
- *   1: {
+ *   534351: {
  *     DAI: {
  *       address: "0x...",
  *       abi: [...],
@@ -11,6 +11,82 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
  *   },
  * } as const;
  */
-const externalContracts = {} as const;
+const externalContracts = {
+    534351: {
+        VeriBot: {
+            address: "0xc7c63d31808D12b1b4BEfd37CFccd461e9CA6F30",
+            abi: [
+                {
+                  "inputs": [],
+                  "name": "plonkVerifier",
+                  "outputs": [
+                    {
+                      "internalType": "contract BaseUltraVerifier",
+                      "name": "",
+                      "type": "address"
+                    }
+                  ],
+                  "stateMutability": "view",
+                  "type": "function"
+                },
+                {
+                  "inputs": [
+                    {
+                      "internalType": "bytes32",
+                      "name": "",
+                      "type": "bytes32"
+                    }
+                  ],
+                  "name": "proofs",
+                  "outputs": [
+                    {
+                      "internalType": "bool",
+                      "name": "",
+                      "type": "bool"
+                    }
+                  ],
+                  "stateMutability": "view",
+                  "type": "function"
+                },
+                {
+                  "inputs": [
+                    {
+                      "internalType": "bytes",
+                      "name": "_proof",
+                      "type": "bytes"
+                    },
+                    {
+                      "internalType": "bytes32[]",
+                      "name": "_publicInputs",
+                      "type": "bytes32[]"
+                    }
+                  ],
+                  "name": "verifyProof",
+                  "outputs": [
+                    {
+                      "internalType": "bool",
+                      "name": "",
+                      "type": "bool"
+                    }
+                  ],
+                  "stateMutability": "nonpayable",
+                  "type": "function"
+                },
+                {
+                  "inputs": [],
+                  "name": "version",
+                  "outputs": [
+                    {
+                      "internalType": "uint256",
+                      "name": "",
+                      "type": "uint256"
+                    }
+                  ],
+                  "stateMutability": "view",
+                  "type": "function"
+                }],
+        }
+    }
+} as const;
 
 export default externalContracts satisfies GenericContractsDeclaration;
